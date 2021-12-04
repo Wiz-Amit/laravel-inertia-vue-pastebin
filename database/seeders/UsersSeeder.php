96b66227a049f67c6abf71ff9b66252fd0467ddb
Amit Hazra <ahazratwt@gmail.com>
2021-12-04 20:47:40 +0530

    + user seeder

---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'John Doe',
+            'email' => 'johndoe@example.com',
+            'password' => 'secret',
+        ]);
+
+        User::factory(10)->create();
+    }
+}
